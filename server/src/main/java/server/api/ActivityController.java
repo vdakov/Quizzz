b3b338b56ac7979ea3de81a1cf81b815ba3d20b2
Vasil Dakov <v.dakov@student.tudelft.nl>
2022-02-28 18:53:35 +0100

    Added PUT and DELETE endpoints

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -10,29 +10,37 @@ import java.util.List;
 @RestController
 @RequestMapping("api/activities")
 public class ActivityController {
-    ActivityRepository repo;
+
+    ActivityRepository repo;//the repository containing the JSONs of activities
+
 
     public ActivityController(ActivityRepository repo) {
         this.repo = repo;
     }
 
+
+    //sends GET HTTP response to client through the ID of the JSON if it exists
     @GetMapping("/{id}")
     public ResponseEntity<Activity> getById(@PathVariable("id") Long id) {
         repo.findAll();
         return !repo.existsById(id) ? ResponseEntity.badRequest().build() : ResponseEntity.ok(repo.getById(id));
     }
 
+    //returns all of the activities to the client in JSON format
     @GetMapping(path = {"", "/"})
     public List<Activity> getAll() {
         return repo.findAll();
     }
 
+
+    //returns a random activity due to a GET request of the cleint
     @GetMapping("/random")
     public Activity getRandom() {
         List<Activity> activities = repo.findAll();
         return activities.get((int) Math.floor(Math.random() * activities.size()));
     }
 
+    //allows the user to add a new activity to the repository and stores it in it permanently through an HTTP POST Request
     @PostMapping(path = {"", "/add"})
     public void add(@RequestBody Activity a) {
         try {
@@ -42,6 +50,42 @@ public class ActivityController {
             throw new IllegalStateException("POST Request Failed");
         }
     }
+
+
+    //allows the user to update all the fields of an activity except the ID, but only if the object with that ID already exists
+    @PutMapping(path="/update/{id}")
+    public void update(@PathVariable ("id") Long id,
+                        @RequestParam(required = false) String title,
+                        @RequestParam(required = false) int consumption,
+                        @RequestParam(required=false) String source){
+
+
+        Activity activity= repo.findById(id).orElseThrow(()-> new IllegalStateException(("No such activity!!!")));
+
+        if(title!= null && title.length()>0){
+            activity.setTitle(title);
+        }
+
+        if(consumption>0){
+            activity.setConsumption(consumption);
+        }
+
+        if(source!=null && source.length()>0){
+            activity.setSource(source);
+        }
+
+        System.out.println(activity.getSource());
+        repo.save(activity);
+
+    }
+
+    //Allows the user to delete an item from the repository if it exists through and HTTP DELETE Request
+    @DeleteMapping(path="/delete/{id}")
+    public void update(@PathVariable ("id") Long id){
+        Activity activity= repo.findById(id).orElseThrow(()-> new IllegalStateException(("No such activity!!!")));
+        repo.delete(activity);
+    }
+
 
 
 
