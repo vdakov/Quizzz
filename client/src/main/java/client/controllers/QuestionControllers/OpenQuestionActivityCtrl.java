2cbc645c7444af52c551476a898bf592e7958e86
Dmytro <D.maksymchuk@student.tudelft.nl>
2022-03-23 23:18:55 +0100

    Deleted old scenes

--- client/src/main/java/client/controllers/QuestionControllers/OpenQuestionActivityCtrl.java
+++ client/src/main/java/client/controllers/QuestionControllers/OpenQuestionActivityCtrl.java
@@ -46,6 +46,7 @@ public class OpenQuestionActivityCtrl extends QuestionActivityCtrl {
     public void initialize()
     {
         answer.setStyle("-fx-background-color: #ffd783; -fx-border-color:  #ffd783; -fx-background-radius: 15; -fx-border-radius: 15;");
+        userAnswerRectangle.setBorder(Border.EMPTY);
         correctAnswerRectangle.setOpacity(0);
     }
 
