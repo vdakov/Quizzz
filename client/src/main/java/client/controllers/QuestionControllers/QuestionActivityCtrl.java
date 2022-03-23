bac6cd91ebe4607ce7079d0854f5dcd83214d808
Dmytro <D.maksymchuk@student.tudelft.nl>
2022-03-23 02:08:09 +0100

    CheckStyle

--- client/src/main/java/client/controllers/QuestionControllers/QuestionActivityCtrl.java
+++ client/src/main/java/client/controllers/QuestionControllers/QuestionActivityCtrl.java
@@ -16,7 +16,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
