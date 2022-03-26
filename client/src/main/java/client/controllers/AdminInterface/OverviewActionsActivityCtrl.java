10547787af27b824c7e76aba01fb01b67115498d
kyongsukim <K.Kim-3@student.tudelft.nl>
2022-03-26 12:26:53 +0100

    Show confirmation alert to delete the activity, where the alert contains the information of activity

--- client/src/main/java/client/controllers/AdminInterface/OverviewActionsActivityCtrl.java
+++ client/src/main/java/client/controllers/AdminInterface/OverviewActionsActivityCtrl.java
@@ -25,10 +25,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -84,6 +81,12 @@ public class OverviewActionsActivityCtrl {
     public void deleteActivity(ActionEvent event) {
         String id = deletingID.getText();
         try {
+            Action deletingAction = server.getActivityById(id);
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Delete Activity");
+            alert.setHeaderText("Are you sure to delete activity");
+            alert.setContentText("ID: "+deletingAction.getId()+"\nTitle: "+deletingAction.getTitle()+"\nConsumption: "+deletingAction.getConsumption());
+            alert.showAndWait();
             server.deleteActivity(id);
         } catch (Exception e) {
             System.out.println("The given ID is not founded");
