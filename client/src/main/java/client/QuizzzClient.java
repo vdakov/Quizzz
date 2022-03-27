773e8cb303176fa1fc71167588e8c20526420a1d
Delia Ion <D.Ion@student.tudelft.nl>
2022-03-27 21:51:44 +0200

    Ckeckstyle

--- client/src/main/java/client/QuizzzClient.java
+++ client/src/main/java/client/QuizzzClient.java
@@ -40,6 +40,7 @@ public class QuizzzClient extends Application {
      */
     public static void main(String[] args) throws URISyntaxException, IOException {
         launch(QuizzzClient.class, args);
+
     }
 
     /**
