0664544f75926149b3fedaa907bc3b17fccdb460
Delia Ion <D.Ion@student.tudelft.nl>
2022-03-27 20:56:08 +0200

    Ckeckstyle

--- client/src/main/java/client/controllers/QuestionControllers/QuestionActivityCtrl.java
+++ client/src/main/java/client/controllers/QuestionControllers/QuestionActivityCtrl.java
@@ -14,7 +14,6 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.skin.SplitPaneSkin;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
