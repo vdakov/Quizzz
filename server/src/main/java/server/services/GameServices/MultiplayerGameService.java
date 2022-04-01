d6b97210c4646c2c748c73bd9f8b50f7c183db34
Catalin Brita <c.e.brita@student.tudelft.nl>
2022-04-01 21:16:06 +0200

    Removedtext label + changed room id length

--- server/src/main/java/server/services/GameServices/MultiplayerGameService.java
+++ server/src/main/java/server/services/GameServices/MultiplayerGameService.java
@@ -57,7 +57,7 @@ public class MultiplayerGameService {
      */
     public String createNewMultiPlayerGame(String username) {
         try {
-            String roomId               = Util.getAlphaNumericString(10);
+            String roomId               = Util.getAlphaNumericString(5);
             ActionCatalog actionCatalog = new ActionCatalog(activityRepository.findAll());
 
             List<Pair<Question, String>> questionList = QuestionGenerator.generateQuestions(actionCatalog, 20, 2, 7, new Random());
