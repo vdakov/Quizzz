d43d3aa8ba6eae0985c9e56512905c8bd5d5b785
kyongsukim <K.Kim-3@student.tudelft.nl>
2022-04-08 16:43:59 +0200

    test singleplayer room in server

---
+++ server/src/test/java/server/entities/MultiplayerRoomTest.java
@@ -0,0 +1,7 @@
+package server.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplayerRoomTest {
+
+}
--- server/src/test/java/server/entities/SingleplayerRoomTest.java
+++ server/src/test/java/server/entities/SingleplayerRoomTest.java
@@ -10,6 +10,12 @@ class SingleplayerRoomTest {
     private SingleplayerRoom s = new SingleplayerRoom("1", "1", null);
 
     @Test
+    void testTimeLeft() {
+        s.setTimeLeft(10000);
+        assertEquals(10000, s.getTimeLeft());
+    }
+
+    @Test
     void calculateAddedPointsTestNoPointJokerNoPartialPoint() {
         s.setTimeLeft(10000);
         assertEquals((int) (1 * 10000 / 100), s.calculateAddedPoints(false));
@@ -18,18 +24,40 @@ class SingleplayerRoomTest {
     @Test
     void calculateAddedPointsTestUsePointJokerUsePartialPoint() {
         s.useDoublePointJoker();
+        assertEquals(true, s.getDoublePointJokerUsed());
         s.setTimeLeft(10000);
         assertEquals((int) ((2 * 10000 / 100) / 2), s.calculateAddedPoints(true));
     }
 
     @Test
+    void calculateAddedPointsTestNoPointJokerUsePartialPoint() {
+        s.setTimeLeft(10000);
+        assertEquals((int) ((1 * 10000 / 100) / 2), s.calculateAddedPoints(true));
+    }
+
+    @Test
+    void calculateAddedPointsTestUsePointJokerNoPartialPoint() {
+        s.useDoublePointJoker();
+        s.setTimeLeft(10000);
+        assertEquals((int) (2 * 10000 / 100), s.calculateAddedPoints(false));
+    }
+
+    @Test
     void testUpdatePlayerScore() {
         s.setPlayerScore(100);
-        s.setTimeLeft(10000);
-        s.calculateAddedPoints(false);
+        s.setAddedPoint(100);
+        assertEquals(100, s.getAddedPoints());
         s.updatePlayerScore();
-        assertEquals(100 + (int) (1 * 10000 / 100), s.getPlayerScore());
+        assertEquals(100 + 100, s.getPlayerScore());
     }
+
+    @Test
+    void testHintJoker() {
+        assertEquals(false, s.getHintJokerUsed());
+        s.useHintJoker();
+        assertEquals(true, s.getHintJokerUsed());
+    }
+
 
 
 
