6bcfc886811354049c87ab2cb21b5169d72ccd64
kyongsukim <K.Kim-3@student.tudelft.nl>
2022-04-08 16:34:21 +0200

    test update score

--- server/src/test/java/server/entities/SingleplayerRoomTest.java
+++ server/src/test/java/server/entities/SingleplayerRoomTest.java
@@ -1,32 +1,37 @@
 package server.entities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SingleplayerRoomTest {
 
+    private SingleplayerRoom s = new SingleplayerRoom("1", "1", null);
+
     @Test
     void calculateAddedPointsTestNoPointJokerNoPartialPoint() {
-        SingleplayerRoom s = new SingleplayerRoom("1", "1", null);
-        s.setAddedPoint(0);
         s.setTimeLeft(10000);
         assertEquals((int) (1 * 10000 / 100), s.calculateAddedPoints(false));
     }
 
     @Test
     void calculateAddedPointsTestUsePointJokerUsePartialPoint() {
-        SingleplayerRoom s = new SingleplayerRoom("1", "1", null);
         s.useDoublePointJoker();
         s.setTimeLeft(10000);
         assertEquals((int) ((2 * 10000 / 100) / 2), s.calculateAddedPoints(true));
     }
 
     @Test
-    void resetAddedPointTest() {
-        SingleplayerRoom s = new SingleplayerRoom("1", "1", null);
-        assertEquals(0, s.getAddedPoints());
+    void testUpdatePlayerScore() {
+        s.setPlayerScore(100);
+        s.setTimeLeft(10000);
+        s.calculateAddedPoints(false);
+        s.updatePlayerScore();
+        assertEquals(100 + (int) (1 * 10000 / 100), s.getPlayerScore());
     }
+
+
 
 
 }
